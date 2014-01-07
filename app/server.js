a3bbf6810ed0f8ac5da8a92392058bcd32960d2e
Wenqer <wenqeer@gmail.com>
2014-01-07 02:30:18 +0200

    remove app symlink
    we do this without *symlink trick*

--- app/server.js
+++ app/server.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-var config = require("app/config");
+var config = require("./config");
 var express = require("express");
 var app = express();
 //Use whichever logging system you prefer.
@@ -14,11 +14,11 @@ app.set("view engine", "jade");
 var AUTOUSING_THE_ROUTER_IS_A_NUISANCE = app.router;
 //Load the routes ("controllers" -ish)
 [
-  "app/users/routes",
-  "app/vehicles/routes",
-  "app/customers/routes",
-  "app/deals/routes",
-  "app/site/routes"
+  "./users/routes",
+  "./vehicles/routes",
+  "./customers/routes",
+  "./deals/routes",
+  "./site/routes"
 ].forEach(function (routePath) {
     require(routePath)(app);
 });
@@ -27,7 +27,7 @@ var AUTOUSING_THE_ROUTER_IS_A_NUISANCE = app.router;
 app.use(app.router);
 
 //FINALLY, use any error handlers
-app.use(require("app/middleware").notFound);
+app.use(require("./middleware").notFound);
 
 //Note that there's not much logic in this file.
 //The server should be mostly "glue" code to set things up and
